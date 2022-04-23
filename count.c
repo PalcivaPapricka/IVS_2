fbc5d0e1fd41ca4ca9298080eb5d9f8815ae8438
Dan Vilimovský <104022651+danecek147@users.noreply.github.com>
2022-04-23 19:17:50 +0200

    Innitial commit / count.c count.h test.c
    Test.c je pouze naznaceno mozne reseni, ale neni prilis elegantni, matematicka knihovna neni uplna, chybi jedna matematicka fce a fce na vypisovani math/syntax erroru - ovsem to bych spojil az s implementaci k gui v main fci

---
+++ count.c
@@ -0,0 +1,78 @@
+/*
+    @task       Solution of an IVS team project - creating calculator
+    @team       "ludia bez týmu sem >>>"
+    @members    Kramár Viktor, Chovanec David, Vilimovský Dan
+    @created    23.4.2022
+    @test       Tested with GCC compiler
+*/
+/**
+    @file   count.c
+    @author Dan Vilimovský
+    @brief  Definig mathematical functions from header file count.h
+*/
+
+#include "count.h"
+
+double addition(float num_1, float num_2){
+    return (num_1 + num_2);
+}
+
+double subtraction(float num_1, float num_2){
+    return (num_1 - num_2);
+}
+
+double multiplication(float num_1, float num_2){
+    return (num_1 * num_2);
+}
+
+double division(float num_1, float num_2){
+    if (num_2 = 0){
+        fprintf(stderr, RED "[ERROR]" RST ": Cannot divide by 0.\n");
+        //TODO ukoncit program / math error
+        return 0;
+    }
+    return (num_1 / num_2);
+}
+
+double power(float num_1, int exponent){
+    if (exponent < 0){
+        fprintf(stderr, RED "[ERROR]" RST ": Exponent must be a positive number or zero.\n");
+        //TODO ukoncit program / math error
+        return 0;
+    }
+    if (exponent = 0){
+        return 1;
+    }
+    double result = num_1;
+    for(int i = 0; i < (exponent - 1); i++){
+        result = result * num_1;
+    }
+    return result;
+}
+
+double root(float num_1, int exponent){
+    if (num_1 < 0){
+        fprintf(stderr, RED "[ERROR]" RST ": Cannot count root from negative number.\n");
+        //TODO ukoncit program / math error
+        return 0;
+    }
+    float exp = 1 / exponent;
+    return pow(num_1, exp);
+}
+
+//TODO fce povinne ze zadani navic
+
+unsigned long factorial(int num){
+    if (num < 0){
+        fprintf(stderr, RED "[ERROR]" RST ": Cannot count factorial from negative number. \n");
+        //TODO ukoncit program / math error
+        return 0; 
+    }
+    unsigned long result = num;
+    int counter = num - 1;
+    while(counter > 1){
+        result = result * counter;
+        counter--;
+    }
+    return result;
+}
---
+++ test.c
@@ -0,0 +1,84 @@
+/*
+    @task       Solution of an IVS team project - creating calculator
+    @team       "ludia bez týmu sem >>>"
+    @members    Kramár Viktor, Chovanec David, Vilimovský Dan
+    @created    23.4.2022
+    @test       Tested with GCC compiler
+*/
+/**
+    @file   test.c
+    @author Dan Vilimovský
+    @brief  Programme to test mathematical functions from count.h
+*/
+
+#include "count.h"
+
+int main(){
+    //testing addition
+    int add = 0;
+
+    if(addition(3.141597, -3756.57) < -3753.4283 && addition(3.141597, -3756.57) > -3753.4285){
+        printf(GREEN "#" RST);
+        add++;
+    }
+    else{
+        printf(RED "#" RST);
+    }
+    if(addition(-148.26, 786.5457) < 638.2858 && addition(-148.26, 786.5457) > 638.2856){
+        printf(GREEN "#" RST);
+        add++;
+    }
+    else{
+        printf(RED "#" RST);
+    }
+    if(addition(-98.9987, -57847.5784) < -57946.5760 && addition(-98.9987, -57847.5784) > -57946.5785){
+        printf(GREEN "#" RST);
+        add++;
+    }
+    else{
+        printf(RED "#" RST);
+    }
+
+    printf("\nTesty funkce na sčítání: %d/3\n", add);
+
+    //testing subtraction
+    int sub = 0;
+
+    if(subtraction(3.141597, -3756.57) < 3759.7120 && subtraction(3.141597, -3756.57) > 3759.7110){
+        printf(GREEN "#" RST);
+        sub++;
+    }
+    else{
+        printf(RED "#" RST);
+    }
+    if(subtraction(-148.26, 786.5457) < -934.8050 && subtraction(-148.26, 786.5457) > -934.8065){
+        printf(GREEN "#" RST);
+        sub++;
+    }
+    else{
+        printf(RED "#" RST);
+    }
+    if(subtraction(-98.9987, -57847.5784) < 57748.5810 && subtraction(-98.9987, -57847.5784) > 57748.5785){
+        printf(GREEN "#" RST);
+        sub++;
+    }
+    else{
+        printf(RED "#" RST);
+    }
+
+    printf("\nTesty funkce na odčítání: %d/3\n", sub);
+
+    //testing multiplication
+
+
+    //testing division
+
+
+    //testing power
+
+
+    //testing root
+
+
+    //testing factorial
+}
